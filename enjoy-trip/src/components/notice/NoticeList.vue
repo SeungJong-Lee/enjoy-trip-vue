6aa66272afe2a9acb828cfaea268c38294a79290
maeugel_rosh <maeugel_rosh@naver.com>
2023-05-19 21:50:35 +0900

    fix: notice list 수정

--- enjoy-trip/src/components/notice/NoticeList.vue
+++ enjoy-trip/src/components/notice/NoticeList.vue
@@ -7,50 +7,76 @@
     </b-row>
     <b-row class="mb-1">
       <b-col class="text-right">
-        <b-button variant="outline-primary" @click="moveWrite()">글쓰기</b-button>
+        <b-button variant="outline-primary" @click="moveWrite()"
+          >글쓰기</b-button
+        >
       </b-col>
     </b-row>
     <b-row>
       <b-col>
-        <b-table striped hover :items="articles" :fields="fields" @row-clicked="viewArticle">
+        <b-table
+          id="my-table"
+          striped
+          hover
+          :items="articles"
+          :per-page="perPage"
+          :current-page="currentPage"
+          :fields="fields"
+          @row-clicked="viewArticle"
+        >
         </b-table>
       </b-col>
+    </b-row>
+    <b-row class="justify-content-center">
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-table"
+      ></b-pagination>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import http from "@/api/http";
+import http from '@/api/http';
 export default {
-  name: "NoticeList",
+  name: 'NoticeList',
   components: {},
   data() {
     return {
       articles: [],
       fields: [
-        { key: "noticeNo", label: "글번호", tdClass: "tdClass" },
-        { key: "noticeTitle", label: "제목", tdClass: "tdSubject" },
-        { key: "userId", label: "작성자", tdClass: "tdClass" },
-        { key: "noticeCreateTime", label: "작성일", tdClass: "tdClass" },
-        { key: "noticeHit", label: "조회수", tdClass: "tdClass" },
+        { key: 'noticeNo', label: '글번호', tdClass: 'tdClass' },
+        { key: 'noticeTitle', label: '제목', tdClass: 'tdSubject' },
+        { key: 'userId', label: '작성자', tdClass: 'tdClass' },
+        { key: 'noticeCreateTime', label: '작성일', tdClass: 'tdClass' },
+        { key: 'noticeHit', label: '조회수', tdClass: 'tdClass' },
       ],
+      perPage: 10,
+      currentPage: 1,
     };
   },
   created() {
-    http.get(`/notice/api?pgno=1&key=&word=`).then(({ data }) => {
-      this.articles = data.data;
-      console.log(data.data);
+    http.get(`/notice/api?key=&word=`).then(({ data }) => {
+      this.articles = data;
+      console.log(data);
     });
   },
   methods: {
     moveWrite() {
-      this.$router.push({ name: "noticewrite" });
+      this.$router.push({ name: 'noticewrite' });
     },
     viewArticle(article) {
       this.$router.push({
-        name: "noticeview",
+        name: 'noticeview',
         params: { articleno: article.noticeNo },
       });
+    },
+  },
+  computed: {
+    rows() {
+      return this.articles.length;
     },
   },
 };
