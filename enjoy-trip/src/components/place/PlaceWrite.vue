a453d0dd4ab5bca5bcedbe292b81fbfbe6db5387
SeungJong-Lee <maeugel_rosh@naver.com>
2023-05-19 10:50:11 +0900

    feat: place 글쓰기 구현

--- enjoy-trip/src/components/place/PlaceWrite.vue
+++ enjoy-trip/src/components/place/PlaceWrite.vue
@@ -1,21 +1,117 @@
 <template>
-  <div>
-    <h1>글쓰기 창입니다.</h1>
+  <div class="container">
+    <div class="form-group">
+      <label for="title">제목</label>
+      <input id="title" type="text" class="form-control" v-model="title" />
+    </div>
+    <div class="form-group">
+      <label for="files">파일 선택</label>
+      <div class="custom-file">
+        <input
+          id="files"
+          type="file"
+          multiple
+          class="custom-file-input"
+          @change="handleFileUpload"
+        />
+        <label class="custom-file-label" for="files">{{ fileLabelText }}</label>
+      </div>
+    </div>
+    <div class="preview">
+      <img v-if="imageUrl" :src="imageUrl" alt="Preview" class="preview-image" />
+    </div>
+    <button class="btn btn-primary" @click="submitPost">게시</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'PlaceWrite',
+  name: "PlaceWrite",
   components: {},
   data() {
     return {
-      message: '',
+      message: "",
+      title: "",
+      files: [],
+      fileLabelText: "파일을 선택해주세요",
+      imageUrl: null,
     };
   },
   created() {},
-  methods: {},
+  methods: {
+    handleFileUpload() {
+      this.files = Array.from(event.target.files);
+      console.log(this.files);
+      const uploadFile = this.files[0];
+      if (uploadFile) {
+        this.image = uploadFile;
+        this.imageUrl = URL.createObjectURL(uploadFile);
+        this.imageLabelText = uploadFile.name;
+      } else {
+        this.image = null;
+        this.imageUrl = null;
+        this.imageLabelText = "이미지를 선택해주세요";
+      }
+    },
+    submitPost() {
+      const formData = new FormData();
+      formData.append("placeTitle", this.title);
+      formData.append("placeContent", "123");
+      var loginUser = sessionStorage.getItem("userId");
+      formData.append("userId", loginUser);
+      for (let i = 0; i < this.files.length; i++) {
+        formData.append("upfile", this.files[i]);
+      }
+      setTimeout(() => {
+        axios.post(`http://localhost:8080/enjoytrip/place/api/write`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        this.$router.push("/place");
+      }, 500);
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+}
+
+.custom-file-input {
+  cursor: pointer;
+}
+
+.custom-file-label::after {
+  content: "파일 선택";
+}
+
+.btn-primary {
+  background-color: #007bff;
+  color: #fff;
+}
+.preview {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.preview-image {
+  width: 500px;
+  height: 300px;
+  transform: translateX(-70px);
+}
+</style>
