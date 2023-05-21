52c09ef3e6a34bd89266486cd1ce61aad2240992
qkdk <86948395+qkdk@users.noreply.github.com>
2023-05-21 22:16:54 +0900

    feat: 계획 작성 모달 구현

--- enjoy-trip/src/components/plan/write/PlanWriteDetail.vue
+++ enjoy-trip/src/components/plan/write/PlanWriteDetail.vue
@@ -30,13 +30,31 @@
         </div>
       </div>
       <div class="button-container">
-        <button class="submit-button">계획 만들기</button>
+        <button class="submit-button" @click="openModal">계획 만들기</button>
       </div>
     </div>
     <div class="scroll-container">
       <plan-attraction-list :attractions="selectedAttractions"
                             @place-click="(attraction) => deleteAttraction(attraction)"></plan-attraction-list>
     </div>
+
+    <!--  모달 내용  -->
+    <transition name="modal-fade">
+      <div v-if="isModalOpen" class="modal" @click.self="closeModal">
+        <div class="modal-content">
+          <h2>계획을 만드시겠습니까?</h2>
+          <div style="display: flex; align-items: center; justify-content: space-between; margin-top: 10vh">
+            <div>
+              계획 제목
+            </div>
+            <input/>
+          </div>
+          <div style="margin-top: 5vh; display: flex; justify-content: end">
+            <button class="submit-button">계획 만들기</button>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -52,6 +70,7 @@ export default {
       userId: "",
       startDate: "",
       endDate: "",
+      isModalOpen: false
     }
   },
   created() {
@@ -68,6 +87,21 @@ export default {
   methods: {
     deleteAttraction(attraction) {
       this.$store.commit("setSelectedAttractionsSet", attraction)
+    },
+    openModal() {
+      this.isModalOpen = true;
+      document.body.style.overflow = 'hidden'; // 화면 스크롤 방지
+      document.addEventListener('click', this.outsideClickHandler);
+    },
+    closeModal() {
+      this.isModalOpen = false;
+      document.body.style.overflow = 'auto'; // 화면 스크롤 재활성화
+      document.removeEventListener('click', this.outsideClickHandler);
+    },
+    outsideClickHandler(event) {
+      if (event.target === event.currentTarget) {
+        this.closeModal();
+      }
     }
   }
 }
@@ -99,4 +133,55 @@ export default {
   padding-left: 2vw;
   padding-right: 1vw
 }
+
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 9999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: opacity 0.3s ease;
+}
+
+.modal-fade-enter-active {
+  opacity: 0;
+}
+
+.modal-fade-enter-to {
+  opacity: 1;
+}
+
+.modal-fade-leave-active {
+  opacity: 1;
+}
+
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-content {
+  background-color: #fefefe;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 30vw;
+  max-width: 600px;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.close:hover {
+  color: #000;
+}
+
 </style>
