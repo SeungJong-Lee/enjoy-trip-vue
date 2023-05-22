a7b83a4164065daa301be9fd32a9e9859fc3459a
SeungJong-Lee <maeugel_rosh@naver.com>
2023-05-22 14:06:36 +0900

    fix: join Profile 추가

--- enjoy-trip/src/components/user/SignUpForm.vue
+++ enjoy-trip/src/components/user/SignUpForm.vue
@@ -1,26 +1,39 @@
 <template>
   <div style="display: flex; flex-direction: column">
+    <div class="preview">
+      <img v-if="imageUrl" :src="imageUrl" alt="Preview" class="preview-image" />
+    </div>
+    <div class="form-group">
+      <div class="custom-container">
+        <input id="files" type="file" multiple class="custom-input" @change="handleFileUpload" />
+      </div>
+    </div>
     <div class="custom-container">
       아이디
-      <input type="text" class="custom-input" v-model="formData.userId"/>
+      <input type="text" class="custom-input" v-model="formData.userId" />
     </div>
     <div class="custom-container">
       비밀번호
-      <input type="password" class="custom-input" v-model="formData.userPw"/>
+      <input type="password" class="custom-input" v-model="formData.userPw" />
     </div>
     <div class="custom-container">
       비밀번호 확인
-      <input type="password" class="custom-input" v-model="userPwCheck"/>
+      <input type="password" class="custom-input" v-model="userPwCheck" />
     </div>
     <div class="custom-container">
       이름
-      <input type="text" class="custom-input" v-model="formData.userName"/>
+      <input type="text" class="custom-input" v-model="formData.userName" />
     </div>
     <div class="custom-container">
       이메일
-      <div style="display: flex; width: 100%; height: 100%" >
-        <input type="text" class="custom-input" v-model="formData.userEmail"/>
-        <input type="email" class="custom-second-input" placeholder="ssafy.com" v-model="formData.userDomain"/>
+      <div style="display: flex; width: 100%; height: 100%">
+        <input type="text" class="custom-input" v-model="formData.userEmail" />
+        <input
+          type="email"
+          class="custom-second-input"
+          placeholder="ssafy.com"
+          v-model="formData.userDomain"
+        />
       </div>
     </div>
     <div class="custom-container">
@@ -31,7 +44,8 @@
 </template>
 
 <script>
-import http from "@/api/http";
+// import http from "@/api/http";
+import axios from "axios";
 
 export default {
   name: "JoinForm",
@@ -44,10 +58,27 @@ export default {
         userName: "",
         userEmail: "",
         userDomain: "",
+      },
+      imageUrl: null,
+      files: [],
+      fileLabelText: "파일을 선택해주세요",
+    };
+  },
+  methods: {
+    handleFileUpload() {
+      this.files = Array.from(event.target.files);
+      console.log(this.files);
+      const uploadFile = this.files[0];
+      if (uploadFile) {
+        this.image = uploadFile;
+        this.imageUrl = URL.createObjectURL(uploadFile);
+        this.imageLabelText = uploadFile.name;
+      } else {
+        this.image = null;
+        this.imageUrl = null;
+        this.imageLabelText = "이미지를 선택해주세요";
       }
-    }
-  }
-  , methods: {
+    },
     afterSignInSuccess(data) {
       if (data.result) {
         alert(data.msg);
@@ -58,19 +89,38 @@ export default {
       alert(data);
     },
     submitSignIn() {
+      console.log(JSON.stringify(this.formData));
       if (this.checkPwEqual()) {
-        http.post(`/user/api/join`, this.formData)
-            .then(({data}) => this.afterSignInSuccess(data))
-            .catch(({response}) => this.afterSignInFailure(response.data))
+        const joinData = new FormData();
+        joinData.append("userId", this.formData.userId);
+        joinData.append("userPw", this.formData.userPw);
+        joinData.append("userName", this.formData.userName);
+        joinData.append("userEmail", this.formData.userEmail);
+        joinData.append("userDomain", this.formData.userDomain);
+        joinData.append("file", this.files[0]);
+        axios
+          .post(`http://localhost:8080/enjoytrip/user/api/join`, joinData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            },
+          })
+          .then(({ data }) => this.afterSignInSuccess(data))
+          .catch(({ response }) => this.afterSignInFailure(response.data));
+
+        // http
+        //   .post(`/user/api/join`, this.formData)
+        //   .then(({ data }) => this.afterSignInSuccess(data))
+        //   .catch(({ response }) => this.afterSignInFailure(response.data));
       } else {
-        alert("비밀번호가 일치하지 않습니다.")
+        alert("비밀번호가 일치하지 않습니다.");
       }
     },
     checkPwEqual() {
       return this.userPwCheck === this.formData.userPw;
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -114,7 +164,7 @@ export default {
   width: 100%;
   outline: none;
   color: gray;
-  margin-left: 5px
+  margin-left: 5px;
 }
 
 .custom-second-input::placeholder {
