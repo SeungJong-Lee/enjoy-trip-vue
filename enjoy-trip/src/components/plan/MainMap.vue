f1cee0f67365968afecf8f3ca656c0bc07e4fd22
qkdk <86948395+qkdk@users.noreply.github.com>
2023-05-22 13:43:44 +0900

    feat: 마커 오버레이 기능 구현
    기존 인포 윈도우 삭제

--- enjoy-trip/src/components/plan/MainMap.vue
+++ enjoy-trip/src/components/plan/MainMap.vue
@@ -32,7 +32,7 @@ export default {
     planAttraction() {
       return this.$store.getters.getPlanAttraction;
     },
-    selectedAttractions(){
+    selectedAttractions() {
       return this.$store.getters.getSelectedAttractions;
     }
   },
@@ -64,10 +64,10 @@ export default {
 
     makeLine(newValue) {
       if (newValue.length !== 0) {
-        var linePath = [];
+        let linePath = [];
         newValue.forEach((place) => linePath.push(new window.kakao.maps.LatLng(place.latitude, place.longitude)))
 
-        var polyline = new window.kakao.maps.Polyline({
+        let polyline = new window.kakao.maps.Polyline({
           path: linePath, // 선을 구성하는 좌표배열 입니다
           strokeWeight: 4, // 선의 두께 입니다
           strokeColor: '#0f4773', // 선의 색깔입니다
@@ -102,7 +102,7 @@ export default {
     },
 
     makeMark(element, index) {
-      var imageSrc =
+      let imageSrc =
               "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png", // 마커이미지의 주소입니다
           imageSize = new window.kakao.maps.Size(24, 24), // 마커이미지의 크기입니다
           imageOption = {}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
@@ -130,66 +130,90 @@ export default {
       const coordY = element.longitude;
       // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
 
-      var markerImage = new window.kakao.maps.MarkerImage(
+      const markerImage = new window.kakao.maps.MarkerImage(
               imageSrc,
               imageSize,
               imageOption
           ),
           markerPosition = new window.kakao.maps.LatLng(coordX, coordY), // 마커가 표시될 위치입니다
-          content = `<div style="height: 100px;width: 350px ">
-                    <div style="background-color: lightgrey">${element.title}</div>
-                        <div style="display: flex; flex-direction: row">
-                            <img style="width: 75px; height: 75px" src=${element.firstImage}></img>
-                            <div>
-                                <span>${element.addr1}</span>
-                                <br/>
-                                <span>${element.zipCode}</span>
-                                <br/>
-                                <span>${element.tel}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
+          content =
+              `<div style="  display: flex;
+                 flex-direction: row;
+                 align-items: flex-start;
+                 margin-top: 5px;
+                 margin-bottom: 5px;
+                 height: 10vh;
+                 border-radius: 5px;
+                 padding: 10px;
+                 background-color: white;
+                 box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                 transition: transform 0.2s;
+               ">
+                 <div style="  padding-left: 1vw;
+                   display: flex;
+                   flex-direction: column;
+                   flex: 1;
+                   width: 100%;
+                   justify-content: space-between;
+                   align-items: flex-start;
+                 ">
+                   <div style="  flex: 1;
+                     font-weight: bold;
+                     font-size: larger;
+                     display: -webkit-box;
+                     -webkit-line-clamp: 1;
+                     -webkit-box-orient: vertical;
+                     overflow: hidden;
+                     text-overflow: ellipsis;">
+                     ${element.title}
+                   </div>
+                   <div style="  flex: 1;
+                     display: -webkit-box;
+                     -webkit-line-clamp: 1;
+                     -webkit-box-orient: vertical;
+                     overflow: hidden;
+                     text-overflow: ellipsis;">
+                     ${element.addr1}
+                   </div>
+                 </div>
+               </div>`
       return {markerPosition, markerImage, content};
     },
     showMark(positions) {
-      for (var i = 0; i < positions.length; i++) {
-        // 마커를 생성합니다
-        // 시작지점 표시
-        var marker = new window.kakao.maps.Marker({
+      for (let i = 0; i < positions.length; i++) {
+        const marker = new window.kakao.maps.Marker({
           map: this.map, // 마커를 표시할 지도
           position: positions[i].markerPosition, // 마커의 위치
           image: positions[i].markerImage,
         });
 
-        // 마커에 표시할 인포윈도우를 생성합니다
-        var infowindow = new window.kakao.maps.InfoWindow({
+        const customOverlay = new window.kakao.maps.CustomOverlay({
+          position: positions[i].markerPosition,
           content: positions[i].content, // 인포윈도우에 표시할 내용
+          xAnchor: 0.5,
+          yAnchor: 1.2,
         });
 
-        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-        // 이벤트 리스너로는 클로저를 만들어 등록합니다
-        // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
         window.kakao.maps.event.addListener(
             marker,
             "mouseover",
-            this.makeOverListener(this.map, marker, infowindow)
+            this.makeOverListener(customOverlay, this.map)
         );
         window.kakao.maps.event.addListener(
             marker,
             "mouseout",
-            this.makeOutListener(infowindow)
+            this.makeOutListener(customOverlay)
         );
       }
     },
-    makeOverListener(map, marker, infowindow) {
+    makeOverListener(overlay, map) {
       return function () {
-        infowindow.open(map, marker);
+        overlay.setMap(map)
       };
     },
-    makeOutListener(infowindow) {
+    makeOutListener(overlay) {
       return function () {
-        infowindow.close();
+        overlay.setMap(null);
       };
     },
   }
