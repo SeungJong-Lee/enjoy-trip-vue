6615223667b99c8fac75aa7fe7c8a64268c8ee2d
qkdk <86948395+qkdk@users.noreply.github.com>
2023-05-25 11:46:12 +0900

    faet: axios 인터셉터 추가

--- enjoy-trip/src/api/http.js
+++ enjoy-trip/src/api/http.js
@@ -1,23 +1,51 @@
 import axios from 'axios';
+import router from "@/router";
 
 // const baseURL = "http://192.168.208.55:8080/enjoytrip";
 const baseURL = 'http://localhost:8080/enjoytrip';
 
 const http = axios.create({
-  baseURL: `${baseURL}`,
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8',
-  },
+    baseURL: `${baseURL}`,
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
 });
 
 function axiosBuilderWithJwt() {
-  return axios.create({
-    baseURL: `${baseURL}`,
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
-    },
-  });
+    const aa = axios.create({
+        baseURL: `${baseURL}`,
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+        },
+    });
+
+    aa.interceptors.response.use(
+        response => response,
+        error => {
+            if (error.response.status === 401) {
+                alert("로그인이 필요합니다.");
+                router.push("/");
+            } else {
+                return Promise.reject(error);
+            }
+        }
+    );
+
+    return aa;
 }
 
-export { baseURL, http, axiosBuilderWithJwt };
+http.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status === 401) {
+            alert("로그인이 필요합니다.")
+            router.push("/");
+        } else {
+            return Promise.reject(error);
+        }
+    }
+);
+
+
+export {baseURL, http, axiosBuilderWithJwt};
